52880df5932856c2171b72edc103a2bc8491994c
Prasanth Reddy <prasanthreddy1406@gmail.com>
2026-02-18 18:57:35 +0530

    Test

--- sample.py
+++ sample.py
@@ -7,6 +7,14 @@ def subtraction(a,b) :
 def multiplication(a,b):
     mul=a*b
     return mul
+def division(a,b) :
+    try :
+        value=a/b;
+        return value
+    except ValueError:
+        print ("please enter a number instead of string ..") 
+
 print(addition(10,5))
 print(subtraction(10,5))
 print(multiplication(10,5))
+print(division(10,5))
